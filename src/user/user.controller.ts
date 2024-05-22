9bedbc7bc3b01fe282c09a28747b410689119e53
Strayneko <bayurendi10@gmail.com>
2024-05-22 21:37:14 +0700

    Feat: controller to fetch user info and store user info

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserRequestDto } from './dto/user-request.dto';
+import { StoreUserRequestDto } from './dto/store-user-request.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('/')
-  public async getUsers() {
-    return await this.userService.getUsers();
+  @Get('/getUserInfo')
+  public async getUsers(@Body() body: UserRequestDto) {
+    return await this.userService.getUserInformation(body);
+  }
+
+  @Post('/storeUserInfo')
+  public async storeUserInfo(@Body() body: StoreUserRequestDto) {
+    return await this.userService.storeUserInformation(body);
   }
 }
