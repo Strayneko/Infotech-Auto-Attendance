c091a72915e1615a829cd8f2596d6fedf8ad5567
Strayneko <bayurendi10@gmail.com>
2024-05-22 17:47:43 +0700

    Refac: move prisma service to module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EncryptionModule } from './encryption/encryption.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [EncryptionModule, PrismaModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
