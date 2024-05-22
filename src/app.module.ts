a8c018bc016bdabd85c968f3c224e5fbb70381b7
Strayneko <bayurendi10@gmail.com>
2024-05-22 17:56:43 +0700

    Feat(WIP): create task scheduler module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { EncryptionModule } from './encryption/encryption.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [EncryptionModule, PrismaModule, UserModule],
+  imports: [
+    EncryptionModule,
+    PrismaModule,
+    UserModule,
+    ScheduleModule.forRoot(),
+    TaskModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/task/task.service.ts
@@ -0,0 +1,13 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+
+@Injectable()
+export class TaskService {
+  private readonly logger = new Logger(TaskService.name);
+
+  @Cron(CronExpression.EVERY_30_SECONDS)
+  handleCron() {
+    console.log('scheduler called');
+    this.logger.debug('Called every 30 seconds');
+  }
+}
