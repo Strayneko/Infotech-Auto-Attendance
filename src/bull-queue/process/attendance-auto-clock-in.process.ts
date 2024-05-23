ca4bb29fda485d4a1735b4d9944febe74f757b9b
Strayneko <bayurendi10@gmail.com>
2024-05-24 06:10:44 +0700

    Feat: create auto clock in queue processor

---
+++ src/bull-queue/process/attendance-auto-clock-in.process.ts
@@ -0,0 +1,13 @@
+import { Process, Processor } from '@nestjs/bull';
+import { Job } from 'bull';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+
+@Processor('ATTENDANCE_CLOCK_IN')
+export class AttendanceAutoClockInProcess {
+  public constructor(private readonly eventEmitter: EventEmitter2) {}
+
+  @Process('auto-clock-in')
+  public handleAttendanceClockIn(job: Job) {
+    this.eventEmitter.emit('autoClockIn:dispatch', job.data);
+  }
+}
