3fb8e363ff4eea685a79aae3c3261857426b2222
Strayneko <bayurendi10@gmail.com>
2024-05-24 05:05:10 +0700

    Feat: add bull queue module configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,9 @@ import { TaskModule } from './task/task.module';
 import { ApiModule } from './api/api.module';
 import { AttendanceModule } from './attendance/attendance.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { BullModule } from '@nestjs/bull';
+import { BullBoardModule } from '@nestql/bull-board';
+import { BullQueueModule } from './bull-queue/bull-queue.module';
 
 @Module({
   imports: [
@@ -20,6 +23,14 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
     EventEmitterModule.forRoot({
       delimiter: ':',
     }),
+    BullModule.forRoot({
+      redis: {
+        host: process.env.REDIS_HOST,
+        port: +process.env.REDIS_PORT || 6379,
+      },
+    }),
+    BullBoardModule.register(),
+    BullQueueModule,
   ],
   controllers: [],
   providers: [],
