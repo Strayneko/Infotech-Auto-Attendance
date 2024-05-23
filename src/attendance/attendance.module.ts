a422ab75d56c1e25d0d917df1a56e5ef24293ea3
Strayneko <bayurendi10@gmail.com>
2024-05-24 06:10:28 +0700

    Feat: dispatch autoclock in bull queue in task scheduler

--- src/attendance/attendance.module.ts
+++ src/attendance/attendance.module.ts
@@ -4,6 +4,7 @@ import { AttendanceController } from './attendance.controller';
 
 @Module({
   providers: [AttendanceService],
+  exports: [AttendanceService],
   controllers: [AttendanceController],
 })
 export class AttendanceModule {}
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,13 +1,27 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { AttendanceService } from '../attendance/attendance.service';
+import { BullQueueService } from '../bull-queue/bull-queue.service';
 
 @Injectable()
 export class TaskService {
   private readonly logger = new Logger(TaskService.name);
 
+  public constructor(
+    private readonly attendanceService: AttendanceService,
+    private readonly bullQueueService: BullQueueService,
+  ) {}
+
   @Cron(CronExpression.EVERY_30_SECONDS)
-  handleCron() {
+  public async handleCron() {
+    const attendances =
+      await this.attendanceService.getAttendanceRequiredData();
+
     console.log('scheduler called');
+    for (const attendance of attendances.data) {
+      await this.bullQueueService.dispatchAutoClockInQueue(attendance);
+    }
+
     this.logger.debug('Called every 30 seconds');
   }
 }
