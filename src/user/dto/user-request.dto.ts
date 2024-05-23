a61dafbce13a5fe31733502e6f825f78fcbb3e9b
Strayneko <bayurendi10@gmail.com>
2024-05-23 22:49:43 +0700

    FeatL add attendance data to user request dto

--- src/user/dto/user-request.dto.ts
+++ src/user/dto/user-request.dto.ts
@@ -1,4 +1,8 @@
+import { AttendanceDataRequestDto } from '../../attendance/dto/attendance-data-request.dto';
+
 export class UserRequestDto {
+  public id?: number;
+
   public email: string;
 
   public deviceId: string;
@@ -18,4 +22,6 @@ export class UserRequestDto {
   public companyId: number;
 
   public infotechUserId: number;
+
+  public attendanceData?: AttendanceDataRequestDto;
 }
