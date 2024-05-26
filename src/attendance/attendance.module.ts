1a0fe985c917ed575d8d47369a5d0198a901d320
Strayneko <bayurendi10@gmail.com>
2024-05-27 01:17:23 +0700

    Feat: remove user module from attendance module

--- src/attendance/attendance.module.ts
+++ src/attendance/attendance.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AttendanceService } from './attendance.service';
 import { AttendanceController } from './attendance.controller';
-import { UserModule } from '../user/user.module';
 
 @Module({
   providers: [AttendanceService],
   exports: [AttendanceService],
   controllers: [AttendanceController],
-  imports: [UserModule],
 })
 export class AttendanceModule {}
