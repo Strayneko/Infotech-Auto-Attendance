75e8fd19d60e65b5a8e002bb8ff2cc271d749a79
Strayneko <bayurendi10@gmail.com>
2024-05-27 01:18:52 +0700

    Feat: import attendance module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { AttendanceModule } from '../attendance/attendance.module';
 
 @Module({
   providers: [UserService],
   exports: [UserService],
   controllers: [UserController],
+  imports: [AttendanceModule],
 })
 export class UserModule {}
