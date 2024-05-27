4fc6ad1ab5384355f786628b6ad1ee8847a94ff8
Strayneko <bayurendi10@gmail.com>
2024-05-27 08:29:49 +0700

    Refac: change scheduler time to utc

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -13,14 +13,14 @@ export class TaskService {
   ) {}
 
   // called monday - friday every 8:25 am asia/jakarta
-  @Cron('0 25 8 * * 1-5')
+  @Cron('0 25 1 * * 1-5')
   public async handleClockInCron() {
     this.logger.log('tes');
     // await this.dispatchClockInOrClockOutJob();
   }
 
   // called monday - friday every 5:30 pm asia/jakarta
-  @Cron('0 30 17 * * 1-5')
+  @Cron('0 30 10 * * 1-5')
   public async handleClockOutCron() {
     await this.dispatchClockInOrClockOutJob();
   }
