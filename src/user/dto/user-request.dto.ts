574eb129501541432de34ae8396512950bc156e8
Strayneko <bayurendi10@gmail.com>
2024-06-04 23:27:23 +0700

    Feat: implement class validator

--- src/user/dto/user-request.dto.ts
+++ src/user/dto/user-request.dto.ts
@@ -1,29 +1,44 @@
 import { AttendanceDataRequestDto } from '../../attendance/dto/attendance-data-request.dto';
+import { IsNotEmpty, ValidateNested } from 'class-validator';
+import { Transform, Type } from 'class-transformer';
 
 export class UserRequestDto {
   public id?: number;
 
+  @IsNotEmpty()
   public email: string;
 
+  @IsNotEmpty()
   public deviceId: string;
 
+  @IsNotEmpty()
   public imei: string;
 
+  @IsNotEmpty()
   public token: string;
 
+  @IsNotEmpty()
+  @Transform((param) => param.value.toString())
   public customerId: string;
 
+  @IsNotEmpty()
   public idNumber: string;
 
+  @IsNotEmpty()
   public userGroupId?: number;
 
+  @IsNotEmpty()
   public employeeId: string;
 
+  @IsNotEmpty()
   public companyId: number;
 
+  @IsNotEmpty()
   public infotechUserId: number;
 
-  public attendanceData?: AttendanceDataRequestDto;
+  @ValidateNested()
+  @Type(() => AttendanceDataRequestDto)
+  public attendanceData!: AttendanceDataRequestDto;
 
   public type?: 'Clock In' | 'Clock Out';
 }
