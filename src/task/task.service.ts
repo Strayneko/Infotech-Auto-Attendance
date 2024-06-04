9925d32e014002d93ab416a82d6898b7cf5fbae0
Strayneko <bayurendi10@gmail.com>
2024-06-05 00:18:47 +0700

    Refac: refactor task scheduler

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,49 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { AttendanceService } from '../attendance/attendance.service';
-import { BullQueueService } from '../bull-queue/bull-queue.service';
 import { MyLoggerService } from '../my-logger/my-logger.service';
-import { Constants } from '../constants';
 
 @Injectable()
 export class TaskService {
   public constructor(
     private readonly attendanceService: AttendanceService,
-    private readonly bullQueueService: BullQueueService,
     private readonly logger: MyLoggerService,
   ) {}
 
   // called monday - friday every 8:25 am asia/jakarta
-  @Cron('0 40 2 * * 1-5')
+  @Cron('0 10 8 * * 1-5')
   public async handleClockInCron() {
-    this.logger.log('tes');
-    await this.dispatchClockInOrClockOutJob('Clock In');
+    this.logger.log('Start clock in cron job');
+    await this.attendanceService.dispatchClockInOrClockOutJob('Clock In');
   }
 
   // called monday - friday every 5:30 pm asia/jakarta
-  @Cron('0 35 17 * * 1-5')
+  @Cron('0 31 17 * * 1-5')
   public async handleClockOutCron() {
-    this.logger.log('Start clocking out cron job');
-    await this.dispatchClockInOrClockOutJob('Clock Out');
-  }
-
-  private async dispatchClockInOrClockOutJob(type: string): Promise<void> {
-    const attendances =
-      await this.attendanceService.getAttendanceRequiredData();
-    console.log(attendances);
-
-    for (const attendance of attendances.data) {
-      // get random delay from 5 seconds to 10 minutes
-      const delay: number =
-        Math.floor(Math.random() * Constants.TEN_MINUTES) +
-        Constants.FIVE_SECONDS;
-      await this.bullQueueService.dispatchAutoClockInQueue(
-        {
-          ...attendance,
-          type,
-        },
-        { delay },
-      );
-    }
+    this.logger.log('Start clock out cron job');
+    await this.attendanceService.dispatchClockInOrClockOutJob('Clock Out');
   }
 }
