b2856e9965fe9be7c89b8e212993085f83ffb696
Strayneko <bayurendi10@gmail.com>
2024-06-06 02:00:27 +0700

    Feat: revert ssl config

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ValidationExceptionFilter } from './filters/validation-exception.filter';
-import * as fs from 'node:fs';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('/etc/ssl/private/attendance-private.pem'),
-    cert: fs.readFileSync('/etc/ssl/certs/attendance.pem'),
-  };
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
