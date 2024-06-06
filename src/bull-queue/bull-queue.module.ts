d655761686ace4c930dcaa765ef4f2e6c4b4427c
Strayneko <bayurendi10@gmail.com>
2024-06-07 02:34:45 +0700

    Feat: create mailer queue job

--- src/bull-queue/bull-queue.module.ts
+++ src/bull-queue/bull-queue.module.ts
@@ -3,15 +3,19 @@ import { BullQueueService } from './bull-queue.service';
 import { BullModule } from '@nestjs/bull';
 import { AttendanceAutoClockInProcess } from './process/attendance-auto-clock-in.process';
 import { MyLoggerModule } from '../my-logger/my-logger.module';
+import { SendMailProcess } from './process/send-mail.process';
 
 @Global()
 @Module({
-  providers: [BullQueueService, AttendanceAutoClockInProcess],
+  providers: [BullQueueService, AttendanceAutoClockInProcess, SendMailProcess],
   exports: [BullQueueService],
   imports: [
     MyLoggerModule.register({ serviceName: BullQueueService.name }),
     BullModule.registerQueue({
       name: 'ATTENDANCE_CLOCK_IN',
+    }),
+    BullModule.registerQueue({
+      name: 'SEND_MAIL_QUEUE',
     }),
   ],
 })
--- src/bull-queue/bull-queue.service.ts
+++ src/bull-queue/bull-queue.service.ts
@@ -1,17 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { InjectQueue } from '@nestjs/bull';
 import { JobOptions, Queue } from 'bull';
+import { MyLoggerService } from '../my-logger/my-logger.service';
 
 @Injectable()
 export class BullQueueService {
   public constructor(
     @InjectQueue('ATTENDANCE_CLOCK_IN') private readonly attendanceQueue: Queue,
+    @InjectQueue('SEND_MAIL_QUEUE') private readonly mailQueue: Queue,
+    private readonly logger: MyLoggerService,
   ) {}
 
-  public async dispatchAutoClockInQueue(
-    data: object,
-    settings: JobOptions = {},
-  ) {
+  public async dispatchAutoClockInQueue(data: any, settings: JobOptions = {}) {
+    this.logger.log(
+      `${data.type} job added for ${data.email} in ${settings.delay / 1000}s`,
+    );
+
+    const delayInMinutes = +settings.delay / 1000 / 60;
+    await this.dispatchMailQueue({
+      recipient: data.email,
+      subject: `You'll be ${data.type} in ${Math.round(delayInMinutes)} minutes.`,
+    });
     await this.attendanceQueue.add('auto-clock-in', data, settings);
+  }
+
+  public async dispatchMailQueue(data: any, settings: JobOptions = {}) {
+    this.logger.log(`Mailing job added for ${data.recipient}`);
+    await this.mailQueue.add('send-mail', data, settings);
   }
 }
