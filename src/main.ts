c838b3167f41a9c24af2819b8922d4704b4247f6
Strayneko <bayurendi10@gmail.com>
2024-06-07 11:35:43 +0700

    Feat: create custom unauthorized exception

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ValidationExceptionFilter } from './filters/validation-exception.filter';
+import { UnauthorizedExceptionFilter } from './filters/unauthorized-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new ValidationExceptionFilter());
+  app.useGlobalFilters(new UnauthorizedExceptionFilter());
   await app.listen(+process.env.SERVER_PORT || 3000);
 }
 bootstrap();
