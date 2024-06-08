ba362b2969dbfdbf14f1df1df31ec870cdca30f2
Strayneko <bayurendi10@gmail.com>
2024-06-08 19:04:51 +0700

    Feat: add attendance history query dto

---
+++ src/attendance/attendance-history-query.dto.ts
@@ -0,0 +1,16 @@
+import { IsIn, IsNumber, IsOptional, Min } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class AttendanceHistoryQueryDto {
+  @Transform((param) => +param.value)
+  @IsOptional()
+  @IsNumber()
+  @IsIn([0, 1])
+  public getLast?: number;
+
+  @Transform((param) => +param.value)
+  @IsNumber()
+  @IsOptional()
+  @Min(1)
+  public page?: number;
+}
