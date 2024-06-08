0ca738c90c6fa2cdd4ea16735036d1642242330d
Strayneko <bayurendi10@gmail.com>
2024-06-08 19:04:22 +0700

    Feat(Docs): comments detail

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -9,7 +9,7 @@ export class TaskService {
     private readonly attendanceService: AttendanceService,
     private readonly logger: MyLoggerService,
   ) {}
-  // called monday - friday every 8:25 am asia/jakarta
+  // called monday - friday every 8:1 am asia/jakarta
   @Cron('0 10 1 * * 1-5')
   public async handleClockInCron() {
     this.logger.log('Start clock in cron job');
