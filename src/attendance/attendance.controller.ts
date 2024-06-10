d70ee75723cc20c6e23796a3303e0b3f9698657f
Strayneko <bayurendi10@gmail.com>
2024-06-10 22:12:22 +0700

    Feat: implement cron job based on user location

--- src/attendance/attendance.controller.ts
+++ src/attendance/attendance.controller.ts
@@ -7,12 +7,14 @@ import {
   Get,
   Param,
   UseGuards,
+  HttpStatus,
 } from '@nestjs/common';
 import { AttendanceService } from './attendance.service';
 import { GetAttendanceHistoryRequestDto } from './dto/get-attendance-history-request.dto';
 import { Response } from 'express';
 import { OneTimeTokenGuard } from '../one-time-token/one-time-token.guard';
 import { AttendanceHistoryQueryDto } from './attendance-history-query.dto';
+import { UserLocationEnum } from './enums/user-location.enum';
 
 @Controller('attendance')
 export class AttendanceController {
@@ -49,10 +51,33 @@ export class AttendanceController {
     return res.status(data.code).json(data);
   }
 
-  @Get('cron/:type')
-  public async handleCron(@Param('type') type: string) {
+  @Get('cron/:type/:location')
+  public async handleCron(
+    @Param('type') type: string,
+    @Param('location') location: string,
+    @Res() res: Response,
+  ) {
     const clockType = type == 'in' ? 'Clock In' : 'Clock Out';
-    await this.attendanceService.dispatchClockInOrClockOutJob(clockType);
-    return { status: true };
+    if (!['indonesia', 'malaysia'].includes(location.toLowerCase())) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        status: false,
+        code: HttpStatus.BAD_REQUEST,
+        message: 'Invalid Location.',
+      });
+    }
+
+    const locationId =
+      location.toLowerCase() === 'indonesia'
+        ? UserLocationEnum.INDONESIA
+        : UserLocationEnum.MALAYSIA;
+    await this.attendanceService.dispatchClockInOrClockOutJob(
+      clockType,
+      locationId,
+    );
+    return res.status(HttpStatus.OK).json({
+      status: true,
+      code: HttpStatus.OK,
+      message: `Clock ${type} cron job dispatched for ${location} users`,
+    });
   }
 }
