2ce0bc47c83d222e75ed72651ec01471d5c7e004
Strayneko <bayurendi10@gmail.com>
2024-06-13 03:56:05 +0700

    Feat: setup sentry exception

---
+++ src/filters/sentry-exeption.filter.ts
@@ -0,0 +1,11 @@
+import { Catch, ArgumentsHost } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import * as Sentry from '@sentry/node';
+
+@Catch()
+export class SentryExeptionFilter extends BaseExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    Sentry.captureException(exception);
+    super.catch(exception, host);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ValidationExceptionFilter } from './filters/validation-exception.filter';
 import { UnauthorizedExceptionFilter } from './filters/unauthorized-exception.filter';
 import { GlobalExceptionFilter } from './filters/global-exception.filter';
+import * as Sentry from '@sentry/node';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
+import { SentryExeptionFilter } from './filters/sentry-exeption.filter';
 
 async function bootstrap() {
+  Sentry.init({
+    dsn: process.env.SENTRY_DNS,
+    integrations: [nodeProfilingIntegration()],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0,
+  });
+
   const app = await NestFactory.create(AppModule);
+  const { httpAdapter } = app.get(HttpAdapterHost);
   app.enableCors({
     allowedHeaders: ['Content-Type', 'X-App-Token', 'X-Request-Time', '*'],
     origin: [
@@ -17,6 +31,7 @@ async function bootstrap() {
     ],
   });
   app.setGlobalPrefix('api');
+  app.useGlobalFilters(new SentryExeptionFilter(httpAdapter));
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new ValidationExceptionFilter());
